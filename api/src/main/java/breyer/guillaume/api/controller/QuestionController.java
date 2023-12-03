ff8217479bf9a97a0d00d11ec99bb9c09988f792
Guillaume <breyerguillaume@gmail.com>
2023-12-03 19:39:17 +0100

    Adding RequestMapping

--- api/src/main/java/breyer/guillaume/api/controller/QuestionController.java
+++ api/src/main/java/breyer/guillaume/api/controller/QuestionController.java
@@ -3,24 +3,27 @@ package breyer.guillaume.api.controller;
 import breyer.guillaume.api.model.Question;
 import breyer.guillaume.api.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping(QuestionController.BASE_URL)
 public class QuestionController {
+
+    public static final String BASE_URL = "/api/v1/questions";
 
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/questions")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<Question> getQuestions() {
         return questionService.getQuestions();
     }
 
-    @GetMapping("/question/{id}")
+    @GetMapping("/{id}")
     public Optional<Question> getQuestion(@PathVariable Long id) {
         return questionService.getQuestion(id);
     }
