d7d2d3abd9cd02cec62255f95013a12ffa4f49d7
Guillaume <breyerguillaume@gmail.com>
2023-12-03 17:19:03 +0100

    Adding Test

---
+++ api/src/test/java/breyer/guillaume/api/controller/QuestionControllerTest.java
@@ -0,0 +1,26 @@
+package breyer.guillaume.api.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class QuestionControllerTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Test
+    void getQuestions() throws Exception {
+        mockMvc.perform(get("/questions")).andExpect(status().isOk()).andExpect(jsonPath("$[0].question",is("Can you talk about you ?")));
+    }
+}
